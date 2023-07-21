89786f819aa7dbcf35d999059a92f2e908651147
Andrei Borzenkov <andreyborzenkov2002@gmail.com>
2023-07-21 18:19:00 +0400

    draft

--- src/Main.hs
+++ src/Main.hs
@@ -22,7 +22,6 @@ import System.Exit
 
 import Pages.DH (generateDhPage)
 import Pages.Common
-import Pages.Graph (todo_cairo_example)
 
 main :: IO ()
 main = do
@@ -34,7 +33,6 @@ main = do
   putByteString (outputDir </> "index.html") (renderMarkup (mainPage time))
   dhPage <- generateDhPage time
   putByteString (outputDir </> "dh.html") (renderMarkup dhPage)
-  todo_cairo_example
 
 putByteString :: FilePath -> ByteString.ByteString -> IO ()
 putByteString path s =
--- src/Pages/Graph.hs
+++ src/Pages/Graph.hs
@@ -5,11 +5,12 @@
 {-# LANGUAGE RecursiveDo #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE ImplicitParams #-}
+{-# LANGUAGE OverloadedRecordDot #-}
 
 module Pages.Graph where
 import Control.Lens
 import Control.Monad.State
-import Control.Arrow ( (>>>) )
 import Data.Generics.Labels ()
 import Data.Set qualified as Set
 import Data.Set ( Set )
@@ -19,7 +20,7 @@ import Data.Text qualified as T
 import Data.Text.Encoding qualified as T
 import Data.Text ( Text )
 import Data.Text.Lazy qualified as T.L
-import Data.Char ( isSpace )
+import Data.Char ( isSpace, isDigit )
 import Data.List qualified
 import Data.ByteString.Lazy qualified as BS.L
 import Data.ByteString qualified as BS
@@ -38,6 +39,9 @@ import qualified GI.Pango as Pango
 import qualified GI.PangoCairo as PangoCairo
 
 import Pages.Common ( cairoFontFamily, cssFontFamily )
+import Data.Traversable
+import Debug.Trace
+import Text.Read
 
 newtype GoalId =
   MkGoalId {
@@ -84,11 +88,12 @@ mkGoal goal subgoals = do
   #dependencies <>= Set.fromList [(goalId, subgoalId) | subgoalId <- subgoals]
   pure goalId
 
-extractToGraphviz :: Roadmap -> Text
-extractToGraphviz MkRoadmap{dependencies, goalLevels, goals} =
+extractToGraphviz :: Roadmap -> GoalTextItems -> Text
+extractToGraphviz MkRoadmap{dependencies, goalLevels, goals} goalTextItems =
   [trimming|
     digraph G {
       ranksep=0.5;
+      dpi = 96;
       node
         [
           shape = box,
@@ -116,17 +121,24 @@ extractToGraphviz MkRoadmap{dependencies, goalLevels, goals} =
   orderGoals = renderLevelPoints goalLevelPoints
   goalLevelPoints = map (\x -> "mock" <> showT x) [1 .. length goalLevels]
 
-  extractGoalInfo (showT -> nodeId, Goalpost{completed}) =
+  extractGoalInfo (nodeId, Goalpost{completed}) =
     [trimming|
-      "$nodeId" [
+      "$nodeIdText" [
         label="",
-        size = "0.75,1.25",
+        width = $wSize,
+        height = $hSize,
+        fixedsize = true,
         color="$color"
         ]
       |] where
     color = if completed then "#593fcf" else "#333333"
+    nodeIdText = showT nodeId
+    textItem = goalTextItems ^. at nodeId
+    hSize = showT $ maybe (trace "default\n" 0.75) (pixelsToInches . (.sizeH)) textItem
+    wSize = showT $ maybe 1.25 (pixelsToInches . (.sizeW)) textItem
+    pixelsToInches = (/96)
 
-  extractGoalLevel (goalLevel, map showT >>> semicolonSeparated -> oneRankGoals) =
+  extractGoalLevel (goalLevel, map showT -> semicolonSeparated -> oneRankGoals) =
     [trimming|{ rank="same"; $goalLevel; $oneRankGoals }|]
 
   extractDependencyGraph (showT -> nodeParent, showT -> nodeChild) =
@@ -150,47 +162,76 @@ extractToGraphviz MkRoadmap{dependencies, goalLevels, goals} =
   showT :: Show a => a -> Text
   showT = T.pack . show
 
-extractToSvg :: Roadmap -> IO BS.L.ByteString
-extractToSvg roadmap = do
-  (exitCode, rawSvg) <- readProcessStdout dot
-  -- TODO: preprocess svg to remove <title> nodes
+extractGraphToSvg :: Roadmap -> GoalTextItems -> Cairo.Render BS.L.ByteString
+extractGraphToSvg roadmap goalTextItems = do
+  (exitCode, rawSvg) <- liftIO $ readProcessStdout dot
   case exitCode of
     ExitSuccess ->
       case fromRawXml $ skipDoctype $ skipXml $ BS.L.toStrict rawSvg of
         Left err -> do
-          BS.L.writeFile "error.svg" rawSvg
-          fail err
-        Right nodes ->
-          pure (toLazyByteString $ encode (postProcessSvg =<< nodes))
+          liftIO $ BS.L.writeFile "error.svg" rawSvg
+          liftIO $ fail err
+        Right [_, Element "svg" attrs nodes, _] -> do
+          svg <- traverse (postProcessSvg (parseHeight attrs) goalTextItems) nodes
+          pure (toLazyByteString $ encode (Xmlbf.element "svg" (updateSvgAttrs attrs) (concat svg)))
+        Right _ -> do
+          liftIO $ BS.L.writeFile "error.svg" rawSvg
+          liftIO $ fail "unexpected nodes"
     ExitFailure _ -> do
-      BS.L.writeFile "error.dot" input
-      fail "Cannot create SVG from dot. Dumped dot-file to the ./error.dot"
+      liftIO $ BS.L.writeFile "error.dot" input
+      liftIO $ fail "Cannot create SVG from dot. Dumped dot-file to the ./error.dot"
   where
     dot =  setStdin (byteStringInput input) $ proc "dot" ["-Tsvg"]
-    input = BS.L.fromStrict $ T.encodeUtf8 $ extractToGraphviz roadmap
+    input = BS.L.fromStrict $ T.encodeUtf8 $ extractToGraphviz roadmap goalTextItems
 
-postProcessSvg :: Node -> [Node]
-postProcessSvg = dfpos \_ -> \case
+    updateSvgAttrs attrs = attrs & at "width" .~ Nothing & at "height" .~ Nothing
+
+    parseHeight attrs = case attrs ^. at "height" of
+      Just (T.takeWhile isDigit -> T.unpack -> height) -> read @Double height
+      Nothing -> error "no height in svg"
+
+postProcessSvg :: Double -> GoalTextItems -> Node -> Cairo.Render [Node]
+postProcessSvg height goalTextItems = dfposM \_ -> \case
 
   Element "g" _ childrens -- Remove mock nodes
     | Just (Element _ _ [Text t]) <- Data.List.find isTitle childrens
     , "mock" `T.L.isPrefixOf` t
-    -> mempty
+    -> pure mempty
 
-  Element "svg" attrs childrens -- remove "width" and "heigh" from svg node
-    -> Xmlbf.element "svg" (attrs & at "width" .~ Nothing & at "height" .~ Nothing) childrens
+  (Element "g" attrs childrens) -- Render cairo text
+    | Just "node" <- attrs ^. at "class"
+    , Just (Element _ _ [Text title]) <- Data.List.find isTitle childrens
+    , Just (Element _ polygonAttrs _) <- Data.List.find isPolygon childrens
+    , Just points <- polygonAttrs ^. at "points"
+    , Just goalId <- readMaybe @Int (T.L.unpack title)
+    , Just coords <- parseStartPoint points
+    , Just textItem <- goalTextItems ^. at (MkGoalId goalId)
+    -> do
+      liftIO $ print coords
+      textItem.createText coords
+      pure (Xmlbf.element "g" attrs (filterOut isTitle childrens))
 
   Element "text" attrs childrens -- setup font
     | Just "Times,serif" <- attrs ^. at "font-family"
-    -> Xmlbf.element "text" (attrs & at "font-family" .~ Just cssFontFamily) childrens
+    -> pure $ Xmlbf.element "text" (attrs & at "font-family" .~ Just cssFontFamily) childrens
 
-  x | isTitle x -> mempty -- remove title from nodes
-
-  x -> pure x -- all other stuff should be unchanged
+  x -> pure [x] -- all other stuff should be unchanged
 
   where
     isTitle (Element "title" _ _) = True
     isTitle _ = False
+
+    isPolygon (Element "polygon" _ _) = True
+    isPolygon _ = False
+
+    filterOut p = filter (not . p)
+
+    parseStartPoint (
+      T.takeWhile (/= ' ') ->
+      T.breakOn "," ->
+        ( T.unpack -> x,
+          T.drop 1 -> T.unpack -> y))
+      = (,) <$> (readMaybe @Double x) <*> (fmap (+height) $ readMaybe @Double y)
 
 
 -- | Skip initial ?xml declaration
@@ -207,15 +248,63 @@ skipXml arg =
     bs = skipBlanks arg
     skipBlanks = S8.dropWhile isSpace
 
+data TextItem =
+  MkTextItem {
+    sizeH :: Double,
+    sizeW :: Double,
+    createText :: (Double, Double) -> Cairo.Render ()
+  }
 
-todo_cairo_example :: IO ()
-todo_cairo_example = do
-  fontDesc <- createFontDescription cairoFontFamily 12000
+type Fonts = (?titleFont :: Pango.FontDescription, ?subtitleFont :: Pango.FontDescription)
+type GoalTextItems = Map GoalId TextItem
+
+goalToTextItem :: Fonts => Goal -> Cairo.Render TextItem
+goalToTextItem Goalpost{title, subtitle} = do
+  (_, wTitle, hTitle, renderTitleText)
+    <- createTextLayout ?titleFont title
+  (_, wSubtitle, hSubtitle, renderSubtitleText)
+    <- createTextLayout ?subtitleFont subtitle
+  let
+    sizeW = i2d (max wTitle wSubtitle) + 5
+    sizeH = i2d hTitle * 1.2 + i2d hSubtitle + 5
+    createText = \(x, y) -> do
+      let
+        xTitle = x + 2.5 + (sizeW - i2d wTitle) / 2
+        yTitle = y + 2.5
+        xSubtitle = x + 2.5 + (sizeW - i2d wSubtitle) / 2
+        ySubitle = y + 2.5 + i2d hTitle * 1.2
+      renderTitleText (xTitle, yTitle)
+      renderSubtitleText (xSubtitle, ySubitle)
+  pure MkTextItem{sizeW, sizeH, createText}
+  where
+    i2d = fromIntegral @Int @Double
+
+createGoalTextItems :: Fonts => Roadmap -> Cairo.Render GoalTextItems
+createGoalTextItems roadmap = for roadmap.goals goalToTextItem
+
+dumpRender :: GoalTextItems -> Cairo.Render ()
+dumpRender items = ifor_ items \(MkGoalId goalId) textItem ->
+  textItem.createText (0, fromIntegral goalId * 75)
+
+testGoal :: Goal
+testGoal =  Goalpost {
+      title = "Dependent Haskell",
+      subtitle = "dependent types in GHC",
+      completed = False
+    }
+
+extractToSvg :: Roadmap -> IO BS.L.ByteString
+extractToSvg roadmap = do
+  titleFont <- createFontDescription cairoFontFamily 16000
+  subtitleFont <- createFontDescription cairoFontFamily 12000
+  let ?titleFont = titleFont; ?subtitleFont = subtitleFont
+
   Cairo.withSVGSurface "hello.svg" 20000 20000 \svgSurface -> do
-    Cairo.renderWith svgSurface $ do
-      (_, _, _, renderText) <- createTextLayout fontDesc "Hello, World"
-      renderText (0, 0)
+    graphSvg <- Cairo.renderWith svgSurface do
+      goalTextItems <- createGoalTextItems roadmap
+      extractGraphToSvg roadmap goalTextItems
     Cairo.surfaceFinish svgSurface
+    pure graphSvg
 
 createFontDescription :: Text -> Int -> IO Pango.FontDescription
 createFontDescription fontFamily fontSize = do
