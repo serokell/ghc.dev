ecdb6b1eb7164a29226b5bd372ff87cacf5f9d3f
Kárándi Tamás <tkarandi@gmail.com>
2023-09-29 11:05:40 +0200

    Run terraform 0.13upgrade
    Problem: we'd like to use latest tf version, but the tf state uses 0.12
    
    Solution: go back in time to tf 0.13.7 and apply `terraform 0.13upgrade`
    
    https://developer.hashicorp.com/terraform/language/v1.1.x/upgrade-guides/0-13
    https://registry.terraform.io/providers/hashicorp/aws/latest/docs/guides/version-3-upgrade#domain_validation_options-changed-from-list-to-set
    
    Meanwhile a small change was detected, but following the suggestion
    `terraform state mv 'aws_route53_record.cert_validation[0]'
    'aws_route53_record.cert_validation["ghc.dev"]'` there was no change

--- deployment/main.tf
+++ deployment/main.tf
@@ -53,6 +53,14 @@ POLICY
     // page.
     //error_document = "404.html"
   }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
 }
 
 resource "aws_acm_certificate" "cert" {
@@ -60,16 +68,24 @@ resource "aws_acm_certificate" "cert" {
   validation_method = "DNS"
 }
 resource "aws_route53_record" "cert_validation" {
-  name    = aws_acm_certificate.cert.domain_validation_options[0].resource_record_name
-  type    = aws_acm_certificate.cert.domain_validation_options[0].resource_record_type
+  for_each = {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name  => {
+      name    = dvo.resource_record_name
+      record  = dvo.resource_record_value
+      type    = dvo.resource_record_type
+    }
+  }
+
+  name    = each.value.name
+  type    = each.value.type
   zone_id = aws_route53_zone.zone.zone_id
-  records = [aws_acm_certificate.cert.domain_validation_options[0].resource_record_value]
+  records = [each.value.record]
   ttl     = 60
 }
 
 resource "aws_acm_certificate_validation" "cert" {
   certificate_arn         = aws_acm_certificate.cert.arn
-  validation_record_fqdns = [aws_route53_record.cert_validation.fqdn]
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
 }
 resource "aws_cloudfront_distribution" "www_distribution" {
   // origin is where CloudFront gets its content from.
