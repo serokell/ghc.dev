a7de834eb21bde6ab4ce3937ed2e0eae3d2a7896
Kárándi Tamás <tkarandi@gmail.com>
2023-09-29 17:34:05 +0200

    Introduce terranix
    Problem: we'd like to use the same techstack in all our infra repos
    
    Solution: use terranix instead of HCL

--- deployment/README.md
+++ deployment/README.md
@@ -1,10 +1,9 @@
 This dir contains terraform information for the AWS resources that are needed for the ghc.dev hosting.
 The state is stored in an aws bucket on our account. Don't forget to set AWS_PROFILE to the correct profile.
 
+From the parent directory:
 ```bash
 export AWS_PROFILE=srk-prod
-nix develop ../.#tf
-terraform init
-terraform plan
-terraform apply
+nix run .#tf-plan
+nix run .#tf-apply
 ```
--- flake.nix
+++ flake.nix
@@ -1,15 +1,37 @@
 {
   description = "ghc-dev";
+
+  inputs = {
+    terranix.url = "github:terranix/terranix";
+    terranix.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
   outputs =
-    { self, nixpkgs }:
+    { self, nixpkgs, terranix }:
     let
       system = "x86_64-linux";
       ghc = "ghc92";
       pkgs = nixpkgs.legacyPackages.${system};
+      lib = nixpkgs.lib;
       haskellPackages =
         pkgs.haskell.packages.${ghc}.extend(hself: hsuper: {
           ghc-dev-webgen = haskellPackages.callCabal2nix "ghc-dev-webgen" "${self}/src/" {};
         });
+      terraform = pkgs.terraform;
+      terraformConfiguration = terranix.lib.terranixConfiguration {
+        inherit system;
+        modules = [ ./deployment/main.nix ];
+      };
+      mkTfApp = command: {
+          type = "app";
+          program = toString (pkgs.writers.writeBash "plan" ''
+            if [[ -e config.tf.json ]]; then rm -f config.tf.json; fi
+            cp ${terraformConfiguration} config.tf.json \
+              && ${terraform}/bin/terraform init \
+              && ${terraform}/bin/terraform ${command}
+          '');
+      };
+      mkTfApps = commands: lib.mapAttrs' (name: lib.nameValuePair "tf-${name}") ( lib.genAttrs commands mkTfApp);
     in
     {
       packages.${system} = {
@@ -22,6 +44,7 @@
               "${haskellPackages.ghc-dev-webgen}"/bin/ghc-dev-webgen "$out/out"
             '';
           };
+
       };
       defaultPackage.${system} = self.packages.${system}.ghc-dev;
       devShell.${system} = pkgs.mkShell {
@@ -36,8 +59,12 @@
 
       devShells.${system}.tf = pkgs.mkShell {
         buildInputs = [
-          nixpkgs.legacyPackages.${system}.terraform
+          terraform
         ];
       };
+
+      # nix run .#tf-plan
+      # nix run .#tf-apply
+      apps.${system} = mkTfApps [ "plan" "apply" ];
     };
 }
